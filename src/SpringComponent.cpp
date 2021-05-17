1141975aadc4be974fb7785c7c9b8ab09d9abbf9
Paul Walker <paul@pwjw.com>
2021-05-17 13:58:25 -0400

    Plays music; shows timer; repaints with anumation; etc...

--- src/SpringComponent.cpp
+++ src/SpringComponent.cpp
@@ -1,9 +1,28 @@
 #include "SpringComponent.h"
 
 //==============================================================================
-SpringComponent::SpringComponent()
-{
-    setSize (600, 400);
+SpringComponent::SpringComponent() {
+    setSize(600, 400);
+    setAudioChannels(0,2);
+
+    formatManager.registerBasicFormats();
+
+    auto file = juce::File(
+            "/Users/paul/Desktop/Ambient Video Project/Audio.wav");                                        // [9]
+    auto *reader = formatManager.createReaderFor(file);                    // [10]
+
+    if (reader != nullptr) {
+        auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader,true);
+        transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+        readerSource = std::move(newSource);
+        transportSource.setPosition(20.0);
+        transportSource.start();
+    }
+    startTimerHz(30);
+}
+
+SpringComponent::~SpringComponent() {
+    shutdownAudio();
 }
 
 //==============================================================================
@@ -14,7 +33,8 @@ void SpringComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::Font (16.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    auto p = std::to_string(transportSource.getCurrentPosition());
+    g.drawText (p, getLocalBounds(), juce::Justification::centred, true);
 }
 
 void SpringComponent::resized()
@@ -22,4 +42,26 @@ void SpringComponent::resized()
     // This is called when the SpringComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+}
+
+void SpringComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
+}
+void SpringComponent::releaseResources()
+{
+    transportSource.releaseResources();
+}
+void SpringComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo &b) {
+    if (readerSource.get() == nullptr)
+    {
+        b.clearActiveBufferRegion();
+        return;
+    }
+
+    transportSource.getNextAudioBlock (b);
+}
+
+void SpringComponent::timerCallback() {
+    repaint();
 }
--- src/SpringComponent.h
+++ src/SpringComponent.h
@@ -1,25 +1,34 @@
 #pragma once
 
-// CMake builds don't use an AppConfig.h, so it's safe to include juce module headers
-// directly. If you need to remain compatible with Projucer-generated builds, and
-// have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
-// you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
-#include <juce_gui_extra/juce_gui_extra.h>
+#include <JuceHeader.h>
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class SpringComponent   : public juce::Component
+class SpringComponent   : public juce::AudioAppComponent,
+        juce::Timer
 {
 public:
     //==============================================================================
     SpringComponent();
+    ~SpringComponent() override;
 
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+
+    void prepareToPlay(int, double) override;
+    void releaseResources() override;
+    void getNextAudioBlock(const juce::AudioSourceChannelInfo &) override;
+
+private:
+    void timerCallback() override;
 
 private:
     //==============================================================================
